888ad074dc4bfe97260339acfd45625e48ef0ef6
Kenny D <kdearstine@outlook.com>
2023-10-16 22:59:57 -0400

    Added basic functionality
    Very basic feed screen that shows posts with a name, picture, caption and like count.

--- lib/post.dart
+++ lib/post.dart
@@ -1,16 +1,17 @@
 class Post {
   String caption;
   String date;
+  String username;
   int likeCount;
   final int _postid;
-  var embed;
+  String embed;
   var userID;
-  Post(this._postid, this.userID, this.caption, this.embed, this.date,
+  Post(this.username, this._postid, this.userID, this.caption, this.embed, this.date,
       this.likeCount);
 }
 
 var samplePost = [
-  Post(123, 222222222, "Took a hike!", "hiketrail.jpg", "10/02/2023", 4),
-  Post(321, 186918691, "Went for a swim!", "pool.jpg", "10/03/2023", 43),
-  Post(231, 186918691, "Had a nice walk!", "park.jpg", "10/04/2023", 343),
+  Post('John', 123, 222222222, "Took a hike!", "hiketrail.jpg", "10/02/2023", 4),
+  Post('Bob', 321, 186918691, "Went for a swim!", "pool.jpg", "10/03/2023", 43),
+  Post('Clair', 231, 186918691, "Had a nice walk!", "park.jpg", "10/04/2023", 343),
 ];
