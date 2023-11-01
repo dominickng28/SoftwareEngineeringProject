2dfe2cc7947ed535b610e2e72723ed62f8ad022d
Kenny D <kdearstine@outlook.com>
2023-10-31 23:36:31 -0400

    Updated with main
    Transferred new ui to the updated main, also modified the post class to hopefully communicate with firebase. Also added a post button, no functionality.

--- lib/home_feed.dart
+++ lib/home_feed.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'post.dart';
 
@@ -13,11 +14,11 @@ class _MyFeed extends State<MyFeed> {
   int _currentIndex = 0;
 
   final List<Post> posts = [
-    Post('Kenny', 123, 222222222, "Caught a lot of fish!",
+    Post('Kenny', 'SampleImages/pfp1.jpg', '123', '222222222', "Caught a lot of fish!",
         "SampleImages/fish.jpg", "10/15/2023", 4),
-    Post('Bob', 321, 186918691, "Went for a swim!", "SampleImages/pool.jpg",
+    Post('Bob', 'SampleImages/pfp2.jpeg', '321', '186918691', "Went for a swim!", "SampleImages/pool.jpg",
         "10/03/2023", 43),
-    Post('Clair', 231, 186918691, "Had a nice walk!", "SampleImages/park.jpg",
+    Post('Clair', 'SampleImages/pfp3.jpg', '231', '186918691', "Had a nice walk!", "SampleImages/park.jpg",
         "10/04/2023", 343),
   ];
 
@@ -33,6 +34,7 @@ class _MyFeed extends State<MyFeed> {
       appBar: AppBar(
         title: const Text('Live4You Homefeed'),
       ),
+      backgroundColor: Color.fromARGB(255, 163, 221, 248),
       body: ListView.builder(
         itemCount: posts.length,
         itemBuilder: (BuildContext context, int index) {
@@ -41,37 +43,82 @@ class _MyFeed extends State<MyFeed> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pop(
-              context); // Navigate back to the previous screen (the homepage)
-        },
-        tooltip: 'Back',
-        child: const Icon(Icons.arrow_back),
-      ),
+          },
+         child: Icon(Icons.add),
+         backgroundColor: Colors.white,
+         ),
+
     );
   }
 }
 
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   final Post post;
-  const PostCard({super.key, required this.post});
+
+  PostCard({required this.post});
+
+  @override
+  _PostCardState createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  bool isLiked = false; // Track whether the post is liked
+  bool isExpanded = false; // Track whether the post embed is expanded
+  double imageHeight = 200.0; // Initial height
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Column(
-      children: [
-        ListTile(
-          title: Text(post.username),
-          subtitle: Text(post.caption),
-        ),
-        Image.asset(post.embed),
-        Row(
-          children: [
-            const Icon(Icons.thumbs_up_down),
-            Text(post.likeCount.toString())
-          ],
-        )
-      ],
-    ));
+      color: const Color.fromARGB(255, 55, 190, 253),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Color.fromARGB(255, 2, 23, 117), width: 6.0), // Set the border color and width here
+        borderRadius: BorderRadius.circular(8.0), 
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(widget.post.pfp),
+            ),
+            title: Text(widget.post.username),
+            subtitle: Text(widget.post.caption),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+                imageHeight = isExpanded ? 400.0 : 200.0; // Set your desired expanded and small sizes here
+              });
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 300), // Animation duration
+              height: imageHeight,
+              child: Image.asset(widget.post.embed),
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(isLiked ? Icons.thumb_up : Icons.thumb_up_alt, color: isLiked ? Color.fromARGB(255, 2, 23, 117) : null),
+                onPressed: () {
+                  setState(() {
+                    isLiked = !isLiked;
+                    if (isLiked) {
+                      widget.post.likeCount++;
+                    } else {
+                      widget.post.likeCount--;
+                    }
+                  });
+                },
+              ),
+              Text(widget.post.likeCount.toString()),
+              Spacer(),
+              Padding(padding: EdgeInsets.only(right: 16.0),
+              child: Text(widget.post.date),)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/post.dart
+++ lib/post.dart
@@ -1,17 +1,62 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Post {
   String caption;
   String date;
   String username;
   int likeCount;
-  final int _postid;
+  final String _postid;
   String embed;
-  var userID;
-  Post(this.username, this._postid, this.userID, this.caption, this.embed, this.date,
+  String pfp;
+  String userID;
+  Post(this.username, this.pfp, this._postid, this.userID, this.caption, this.embed, this.date,
       this.likeCount);
+
+  String getCaption(){
+    return caption;
+  }
+  String getDate(){
+    return date;
+  }
+  String getUsername(){
+    return username;
+  }
+  int getLikeCount(){
+    return likeCount;
+  }
+  String _getPostID(){
+    return _postid;
+  }
+  String getEmbed(){
+    return embed;
+  }
+  String getPfp(){
+    return pfp;
+  }
+
+factory Post.fromFirestore(DocumentSnapshot document, String _postid){
+  final data = document.data() as Map<String, dynamic>;
+
+  final post = Post(
+    data['username'],
+    data['pfp'],
+    _postid,
+    data['userID'],
+    data['caption'],
+    data['embed'],
+    data['date'],
+    data['likeCount'],
+    
+    );
+    return post;
+}
+
 }
 
 var samplePost = [
-  Post('John', 123, 222222222, "Took a hike!", "hiketrail.jpg", "10/02/2023", 4),
-  Post('Bob', 321, 186918691, "Went for a swim!", "pool.jpg", "10/03/2023", 43),
-  Post('Clair', 231, 186918691, "Had a nice walk!", "park.jpg", "10/04/2023", 343),
+  Post('John', 'pfp1.jpg', '123', '222222222', "Took a hike!", "hiketrail.jpg", "10/02/2023", 4),
+  Post('Bob', 'pfp2.jpeg', '321', '186918691', "Went for a swim!", "pool.jpg", "10/03/2023", 43),
+  Post('Clair', 'pfp.jpg', '231', '186918691', "Had a nice walk!", "park.jpg", "10/04/2023", 343),
 ];
