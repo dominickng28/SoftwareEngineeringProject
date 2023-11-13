04b9f8bfe8b56813180599946820ad586d7dfb6e
Kenny D <kdearstine@outlook.com>
2023-11-13 16:45:53 -0500

    ui changes and fixes
    Changed the UI a bit, made posts get off of cards. Fixed the like state so that it stays between runs, likeCount is properly updated and updates likePost function to use transactions to cover events like two different people liking the same post at the same time.

--- lib/home_feed.dart
+++ lib/home_feed.dart
@@ -152,10 +152,14 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   bool isLiked = false; // Track whether the post is liked
-  bool isExpanded = false; // Track whether the post embed is expanded
   
-  
-  double imageHeight = 200.0; // Initial height
+  //Check to properly initialize the isLiked variable
+  @override
+  void initState(){
+    super.initState();
+    isLiked = widget.post.likes?.contains(UserData.userName) ?? false;
+  }
+
   String timeAgo(DateTime date) {
     Duration diff = DateTime.now().difference(date);
     if (diff.inDays > 0) {
@@ -173,7 +177,9 @@ class _PostCardState extends State<PostCard> {
   Widget build(BuildContext context) {
     //checking which post is made by the user
     bool isPoster = widget.post.username == UserData.userName;
-    return Card(
+    double screenWidth = MediaQuery.of(context).size.width;
+    double cutOffValue = 0.95;
+    return Container(
       color: const Color.fromARGB(249, 253, 208, 149),
       child: Column(
         children: [
@@ -190,22 +196,12 @@ class _PostCardState extends State<PostCard> {
               onPressed: () => deletePost(context),
             ): null
           ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                isExpanded = !isExpanded;
-                imageHeight = isExpanded
-                    ? imageHeight
-                    : 200.0; // Set your desired expanded and small sizes here
-              });
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 300), // Animation duration
-              width: double.infinity,
-              height: imageHeight,
-              child: Image.network(widget.post.imageUrl),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(widget.post.imageUrl, fit: BoxFit.cover,
             ),
           ),
+          
           Row(
             children: [
               IconButton(
@@ -213,19 +209,24 @@ class _PostCardState extends State<PostCard> {
                     color: isLiked ? Color.fromARGB(255, 2, 23, 117) : null),
                 onPressed: () => likePost(context),
               ),
-              Text((widget.post.likes?.length ?? 0).toString()),
+              Text((widget.post.likeCount).toString()),
               Spacer(),
               Padding(
                 padding: EdgeInsets.only(right: 16.0),
                 child: Text(timeAgo(widget.post.date)),
               ),
-              
             ],
-          )
+          ),
+          Container(
+            height: 2.0,
+            width: screenWidth * cutOffValue,
+            color: Color.fromARGB(248, 172, 113, 36)
+          ),
         ],
       ),
     );
   }
+
   Future<void> deletePost(BuildContext parentContext) async{
     return showDialog(
       context: parentContext,
@@ -252,30 +253,39 @@ class _PostCardState extends State<PostCard> {
             onPressed: () => Navigator.pop(context),
             child: Text('Cancel'),
           )
-
         ],
-        
         );
       }
     );
   }
-  Future<void> likePost(BuildContext parentContext) async {
+
+Future<void> likePost(BuildContext parentContext) async {
   final user = UserData.userName;
   widget.post.likes ??= [];
 
-  if (widget.post.likes!.contains(user)) {
-    //If the user already liked the post, unlike it
-    widget.post.likes!.remove(user);
-  } else {
-    //If the user hasn't liked the post, like it
-    widget.post.likes!.add(user);
-  }
-  //Updates Firestore to new likes
-  await FirebaseFirestore.instance
-      .collection('posts')
-      .doc(widget.post.getPostID())
-      .update({'likes': widget.post.likes ??= []});
-  //Set state to the change in likes
+  //Use a transaction to handle concurrent updates safely
+  await FirebaseFirestore.instance.runTransaction((transaction) async {
+    DocumentSnapshot postSnapshot =
+        await transaction.get(FirebaseFirestore.instance.collection('posts').doc(widget.post.getPostID()));
+
+    //Update likes, increment or decrement if necessary
+    List<String> updatedLikes = List<String>.from(postSnapshot['likes'] ?? []);
+    if (updatedLikes.contains(user)) {
+      updatedLikes.remove(user);
+      widget.post.likeCount--; 
+    } else {
+      updatedLikes.add(user);
+      widget.post.likeCount++; 
+    }
+
+    // Update the document with the new data
+    transaction.update(FirebaseFirestore.instance.collection('posts').doc(widget.post.getPostID()), {
+      'likes': updatedLikes,
+      'likeCount': widget.post.likeCount,
+    });
+  });
+
+  // Set state to the change in likes
   setState(() {
     isLiked = widget.post.likes!.contains(user);
   });
