8d9eee0b6d8dd77b3a4e0da98d2087e829f79ec4
Kenny D <kdearstine@outlook.com>
2023-12-05 01:23:06 -0500

    Update like_testing.dart

--- test/like_testing.dart
+++ test/like_testing.dart
@@ -21,7 +21,6 @@ void main() {
   );
 
   await tester.runAsync(() async {
-    // Pump the PostCard widget with the mock post
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
@@ -56,6 +55,43 @@ void main() {
     // Verify that the post is now unliked
     expect(find.byIcon(Icons.favorite_rounded), findsOneWidget);
     expect(find.byIcon(Icons.favorite), findsNothing);
+  });
+});
+testWidgets('Spamming Like Button', (WidgetTester tester) async {
+  // Create a mock post
+  Post mockPost = Post(
+    'woolly', 'lib/assets/default-user.jpg', 'IFVJ7s8mhsbt1n7I2d5D', '', 'Run', 'run run run', '', DateTime(2023, 11, 28, 10, 40, 28), 1, ['woolly'], 
+    'https://firebasestorage.googleapis.com/v0/b/live-4-you.appspot.com/o/posts%2FCAP6097955994966011154.jpg?alt=media&token=4f6a7573-66d6-4593-96fa-1f803cf218e8'
+  );
+
+  await tester.runAsync(() async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: PostCard(post: mockPost),
+        ),
+      ),
+    );
+
+    // Find the like button in the post
+    Finder likeButtonFinder = find.byIcon(Icons.favorite);
+
+    // Simulate spamming the like button multiple times
+    for (int i = 0; i < 10; i++) {
+      // Tap the like button
+      await tester.tap(likeButtonFinder);
+
+      // Rebuild the widget after the tap
+      await tester.pump();
+
+      // Delay to simulate user interaction frequency
+      await Future.delayed(const Duration(milliseconds: 100));
+    }
+
+    // Verify the final state after spamming
+    bool isLiked = (mockPost.likeCount % 2) == 1; // Toggle like state
+    expect(find.byIcon(isLiked ? Icons.favorite : Icons.favorite_rounded), findsOneWidget);
+    expect(find.byIcon(isLiked ? Icons.favorite_rounded : Icons.favorite), findsNothing);
   });
 });
 }
